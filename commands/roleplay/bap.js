cbf89c553108cc0a81cf9362770e129d0d172fcc
Cayoten <57970060+cayoten@users.noreply.github.com>
2021-07-05 14:48:46 -0500

    Make bap a RP command and actually have them bap a user instead of "you"

---
+++ commands/roleplay/bap.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "bap",
+    usage: "< id / mention >",
+    permlevel: "SEND_MESSAGES",
+    catergory: "miscellaneous",
+    description: `Beans the user tagged.`,
+    run: async function (client, message, args) {
+
+        if (client.cooldownManager.checkCooldownAndNotify("rp", message.author.id, message)) {
+            return;
+        }
+
+        if (message.deletable) message.delete();
+
+        let bapUser = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!bapUser) return message.reply("user not found.");
+        if (message.mentions.members.first().user === message.author) return message.reply("you can't roleplay with yourself!");
+
+        await message.channel.send(`*baps ${bapUser}* \nNO! BAD!`);
+        message.delete().catch();
+
+    }
+
+};
