8d39b4d5dcdb1cde682c84dff46975a0877c08d3
Cayoten <57970060+cayoten@users.noreply.github.com>
2021-07-12 10:42:05 -0500

    Remove paws & talons from lick, as per community request

--- commands/roleplay/lick.js
+++ commands/roleplay/lick.js
@@ -13,7 +13,6 @@ module.exports = {
         if (message.deletable) message.delete();
 
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!args[0]) return message.reply("Please specify whether you want to lick their `face`, `paws`, or `talons`!")
         if (!user) return message.reply("user not found.");
 
         if (message.mentions.members.first().user === message.author) return message.reply("you can't roleplay with yourself!");
@@ -22,17 +21,7 @@ module.exports = {
 
         let array = ["grins and licks", "flops and licks", "gets up and licks", "happily licks", "submissively licks", "smooches and licks", "pushed over and licked"];
 
-        if (args[0] === "face") {
-            await message.channel.send(`${message.author} ${array[Math.round(Math.random() * (array.length - 1))]} ${user}'s face!`);
-        }
-
-        if (args[0] === "paws") {
-            await message.channel.send(`${message.author} ${array[Math.round(Math.random() * (array.length - 1))]} ${user}'s paws!`);
-        }
-
-        if (args[0] === "talons") {
-            await message.channel.send(`${message.author} ${array[Math.round(Math.random() * (array.length - 1))]} ${user}'s talons!`);
-        }
+        await message.channel.send(`${message.author} ${array[Math.round(Math.random() * (array.length - 1))]} ${user}'s face!`);
 
     }
 
