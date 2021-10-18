1e340303625db0f8b7078fe179c82ab1d9aa7589
Cayoten <57970060+cayoten@users.noreply.github.com>
2021-10-18 16:46:51 -0500

    Make 8ball a tiny bit funnier

--- commands/miscellaneous/8ball.js
+++ commands/miscellaneous/8ball.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 //Optimization: we don't need to create this array every time we execute the command.
-const responses = ["Oh HELL nah", "Try again when I feel like answering", "Did you really just ask me that?", "FUCK YEAH", "yes pls", "You know it sugar!", "Never ever in a million years", "idk, ask yourself"];
+const responses = ["Oh HELL nah", "Try again when I feel like answering", "Did you really just ask me that?", "FUCK YEAH", "yes pls", "You know it sugar!", "Never ever in a million years", "sure why not, leave me alone"];
 module.exports = {
     name: "8ball",
     usage: "< question > ",
@@ -24,7 +24,7 @@ module.exports = {
         client.cooldownManager.setCoolDown("8ball", message.author.id, 60);
 
         let ballembed = new Discord.MessageEmbed()
-            .setTitle("Your 8ball answer is:")
+            .setTitle("Your sassy 8ball answer is:")
             .setColor("#f6ca2e")
             .setDescription(`${responses[Math.round(Math.random() * (responses.length - 1))]}`)
             .setFooter("Developed by Cayoten")
